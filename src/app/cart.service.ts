6b2165358393496a4c63a6342686877fbf1694a3
ljhvvs <a2775269@gmail.com>
2025-04-28 16:21:31 +0800

    Initial Commit

---
+++ src/app/cart.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './products';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  items: Product[] = [];
+  constructor(private http: HttpClient) {}
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+  getItems(): Product[] {
+    return this.items;
+  }
+  clearCart(): Product[] {
+    this.items = [];
+    return this.items;
+  }
+}
